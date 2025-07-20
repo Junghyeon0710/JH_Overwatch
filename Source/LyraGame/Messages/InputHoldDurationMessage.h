21aa4b25bd0d0cbf0edc330a014645c29aa73fc2
Junghyeon0710 <sie08357@naver.com>
2025-07-20 18:14:39 +0900

    화살 동적 크로스헤어 구현

---
+++ Source/LyraGame/Messages/InputHoldDurationMessage.h
@@ -0,0 +1,21 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+#include "InputHoldDurationMessage.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FInputHoldDurationMessage
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, Category=Gameplay)
+	TObjectPtr<AActor> Instigator;
+	
+	UPROPERTY(BlueprintReadWrite, Category=Gameplay)
+	float Duration = 0;
+};
